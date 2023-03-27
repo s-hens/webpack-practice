cd80f17c56364cbb1177d1991b49980d50a2f58f
s-hens <108551327+s-hens@users.noreply.github.com>
2023-03-27 15:55:45 +0100

    Include CSS in bundle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {testFunction1} from "./test1";
 import {testFunction2, styling} from "./test2";
+import "./style.css";
 
 testFunction1();
 testFunction2();
 styling();
+
+document.body.classList.add("hello");
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,16 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
